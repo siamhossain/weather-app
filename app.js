71d97c116e684167311554ccf87b32aa931fdcd1
Siam Hossain <siam.hossain1994@gmail.com>
2020-02-12 00:40:22 +0600

    Add api and fetch data

---
+++ app.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+
+            const api = `${proxy}https://api.darksky.net/forecast/affa221d6c9abd01b649ab0c0b9d91c4/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { temperature, summary } = data.currently;
+                });
+        });
+    }
+});
